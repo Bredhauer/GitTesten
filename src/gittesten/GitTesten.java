54a81227668cc0775831bae85da9b78ce496027c
Eugen <ebredhauer@cdtnue.onmicrosoft.com>
2024-01-24 13:50:39 +0100

    Mein erster Commit

---
+++ src/gittesten/GitTesten.java
@@ -0,0 +1,13 @@
+package gittesten;
+
+public class GitTesten {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Ich arbeite heute das erste Mal mit Git");
+        System.out.println("Ein Git Repository habe ich bereits initialisiert");
+        System.out.println("Der Befehl hierfür lautet: git init");
+        
+    }
+    
+}
