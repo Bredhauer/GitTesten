d08c544bd66e69a14e2977bc9b25a52da9564db1
Eugen <ebredhauer@cdtnue.onmicrosoft.com>
2024-01-24 14:03:50 +0100

    GitTesten modifiziert

--- src/gittesten/GitTesten.java
+++ src/gittesten/GitTesten.java
@@ -8,6 +8,14 @@ public class GitTesten {
         System.out.println("Ein Git Repository habe ich bereits initialisiert");
         System.out.println("Der Befehl hierfür lautet: git init");
         
+        System.out.println("Gerade habe ich meinen ersten Commit durchgeführt"
+                + "und damit die aktuelle Version in meinem lokalen Repository gespeichert");
+        System.out.println("Das ging mit dem Befehl \"git commit\"."
+                + "Zuvor musste ich mit \"git add --add\" alle im Projekt enthaltenen Dateien"
+                + "für den Commit hinzufügen");
+        
+                
+        
     }
     
 }
